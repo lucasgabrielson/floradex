c596e8b29cde267bf80bb95ed901a1e695fb9649
lucasgabrielson <lucas.c.gabrielson@gmail.com>
2021-04-19 09:43:38 -0500

    updated database.sql with create table queries

--- database.sql
+++ database.sql
@@ -8,3 +8,28 @@ CREATE TABLE "user" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+CREATE TABLE natural_areas (
+	"id" SERIAL PRIMARY KEY,
+	"dnr_api" VARCHAR (400) UNIQUE NOT NULL
+);
+
+CREATE TABLE user_hunts (
+	"id" SERIAL PRIMARY KEY,
+	"user_id" INT NOT NULL,
+	FOREIGN KEY (user_id) REFERENCES "user"(id),
+	"natural_area_id" VARCHAR(400),
+	FOREIGN KEY (natural_area_id) REFERENCES natural_areas(dnr_api)
+);
+
+CREATE TABLE natural_areas_hunts_flora (
+	"id" SERIAL PRIMARY KEY,
+	common_name VARCHAR(255),
+	scientific_name VARCHAR(255),	
+	"user_id" INT NOT NULL,
+	FOREIGN KEY (user_id) REFERENCES "user"(id),
+	"natural_area_id" VARCHAR(400),
+	FOREIGN KEY (natural_area_id) REFERENCES natural_areas(dnr_api),
+	found BOOLEAN,
+	substantiated BOOLEAN
+);
