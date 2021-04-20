31f1278076eb8702738c8247f1989f30a5b729fc
lucasgabrielson <lucas.c.gabrielson@gmail.com>
2021-04-20 00:13:48 -0500

    add to my hunts button operational

--- server/routes/my-hunts.js
+++ server/routes/my-hunts.js
@@ -14,7 +14,8 @@ router.get('/', (req, res) => {
  */
 router.post('/', (req, res) => {
     console.log('in /api/my-hunts POST', req.query);
-    let queryText = `INSERT INTO user_hunts ("user_id", "natural_area_id") VALUES ($1, $2);`
+    let queryText = `INSERT INTO user_hunts 
+    ("user_id", "natural_area_id") VALUES ($1, $2);`
     pool.query( queryText, [ req.query.userId, 'http://services.dnr.state.mn.us/api/sna/detail/v1?id=' + req.query.endpoint])
         .then( _ => {
             res.sendStatus(200)
