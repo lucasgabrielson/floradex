76652d85e1796545bb9ddb443d7e76c5508dd347
lucasgabrielson <lucas.c.gabrielson@gmail.com>
2021-04-20 20:34:21 -0500

    added modal for details

--- src/components/NaturalAreaItemSpeciesList/NaturalAreaItemSpeciesList.jsx
+++ src/components/NaturalAreaItemSpeciesList/NaturalAreaItemSpeciesList.jsx
@@ -1,8 +1,50 @@
 import React from 'react'
 import TableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+
+function getModalStyle() {
+    return {
+        top: `$45%`,
+        left: `$45%`,
+        transform: `translate(-$45%, -$45%)`,
+    };
+}
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        position: 'absolute',
+        width: 400,
+        backgroundColor: theme.palette.background.paper,
+        border: '2px solid #000',
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2, 4, 3),
+    },
+}));
 
 const NaturalAreaItemSpeciesList = ({row, index, type}) => {
+    const classes = useStyles();
+    // getModalStyle is not a pure function, we roll the style only on the first render
+    const [modalStyle] = React.useState(getModalStyle);
+    const [open, setOpen] = React.useState(false);
+
+    const handleOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+    const body = (
+        <div style={modalStyle} className={classes.paper}>
+        <h2 id="simple-modal-title">Text in a modal</h2>
+        <p id="simple-modal-description">
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+        </p>
+        </div>
+    );
+
     return (
         <>
             <TableRow key={index}>
@@ -10,8 +52,16 @@ const NaturalAreaItemSpeciesList = ({row, index, type}) => {
                     {row.cname}
                 </TableCell>
                 <TableCell align="right">{type}</TableCell>
-                <TableCell align="right">{'Details'}</TableCell>
+                <TableCell align="right" onClick={handleOpen}>{'Details'}</TableCell>
             </TableRow>
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="simple-modal-title"
+                aria-describedby="simple-modal-description"
+                >
+                {body}
+            </Modal>
         </>
     )
 }
